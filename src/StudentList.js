94cb7f154ce591414e966c474ddb6d627f0ef774
Sayam Vashisht <sv6931@srmist.edu.in>
2023-08-29 03:37:13 +0530

    Updated the App.

--- src/StudentList.js
+++ src/StudentList.js
@@ -4,7 +4,7 @@ import StudentItem from './StudentItem';
 function StudentList({ students, toggleAttendance }) {
   return (
     <div className="student-list">
-      <h2>Student List</h2>
+      <h2 style={ {textAlign: "center" , fontSize: 30}}> <u>Student List</u></h2>
       <ul>
         {students.map((student) => (
           <StudentItem key={student.id} student={student} toggleAttendance={toggleAttendance} />
